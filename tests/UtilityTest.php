f554be52cad8b17967f25d03b0204db1f8cb0d5b
Fred Myerscough <oniice@gmail.com>
2026-04-03 13:07:36 +0100

    feat(utility): add make, response, isAlive
    - Add static make() factory method
    - Add response() to expose ResponseUtility
    - Add isAlive() for quick liveness checks
    - Add tests for all new features

--- tests/UtilityTest.php
+++ tests/UtilityTest.php
@@ -6,11 +6,11 @@ namespace Tests;
 
 use Myerscode\Utilities\Web\ContentUtility;
 use Myerscode\Utilities\Web\PingUtility;
+use Myerscode\Utilities\Web\ResponseUtility;
 use Myerscode\Utilities\Web\UriUtility;
 use Myerscode\Utilities\Web\Utility;
-use PHPUnit\Framework\TestCase;
 
-final class UtilityTest extends TestCase
+final class UtilityTest extends BaseSuite
 {
     public function testContentReturnsContentUtility(): void
     {
@@ -19,11 +19,43 @@ final class UtilityTest extends TestCase
         $this->assertInstanceOf(ContentUtility::class, $utility->content());
     }
 
+    public function testIsAliveReturnsFalseFor500(): void
+    {
+        self::$server->setResponseOfPath('/utility-dead', new TestResponse('', [], 500));
+
+        $utility = new Utility(self::serverUrl('/utility-dead'));
+
+        $this->assertFalse($utility->isAlive());
+    }
+
+    public function testIsAliveReturnsTrueFor200(): void
+    {
+        self::$server->setResponseOfPath('/utility-alive', new TestResponse('', [], 200));
+
+        $utility = new Utility(self::serverUrl('/utility-alive'));
+
+        $this->assertTrue($utility->isAlive());
+    }
+
+    public function testMakeReturnsUtilityInstance(): void
+    {
+        $utility = Utility::make('https://example.com');
+
+        $this->assertInstanceOf(Utility::class, $utility);
+    }
+
     public function testPingReturnsPingUtility(): void
     {
         $utility = new Utility('https://example.com');
 
         $this->assertInstanceOf(PingUtility::class, $utility->ping());
+    }
+
+    public function testResponseReturnsResponseUtility(): void
+    {
+        $utility = new Utility('https://example.com');
+
+        $this->assertInstanceOf(ResponseUtility::class, $utility->response());
     }
 
     public function testUrlReturnsUriUtility(): void
